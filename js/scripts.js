af4a8ed19d11c16efa9b79834592ec734d917a1d
Angela Weber and Sarado Touch <sarado@>
2019-10-02 20:40:05 -0700

    attempt at palindrome code

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,67 @@
-var factorial = function(number) {
-  if (number === 0)
-  {
-    return 1;
-  }
-  return number * factorial(number - 1)
+function reverseString(str) {
+    return str.split("").reverse().join("");
+    if reverseString(str) === str {
+      return true;
+    }
 }
+
 
 $(document).ready(function() {
   $('form#leap-year').submit(function(event){
-    var number = parseInt($('input#number').val());
-    var result = factorial(number);
+    var word = $('input#letter').val();
+    var result = reverseString(word);
 
-
-    $('.answer').text(result);
     $('#result').show();
-
+    $('.answer').text(result);
 
     event.preventDefault();
   });
 });
 
 
+
+
+
+
+
+// var palindrome = function(letter) {
+//   if (letter === 0)
+//   {
+//     return 1;
+//   }
+//   return letter * factorial(letter - 1)
+// }
+//
+// // 6! = 6 * 5 * 4 * 3 * 1
 // $(document).ready(function() {
-//   $("form#leap-year").submit(function(event) {
+//   $('form#leap-year').submit(function(event){
+//     var number = parseInt($('input#number').val());
+//     var result = factorial(letter);
+//
+//
+//     $('.answer').text(result);
+//     $('#result').show();
+//
+//
 //     event.preventDefault();
-//     var number = parseInt($("input#number").val());
-//     var result = leapYear(year);
-//
-//     $(".year").text(year);
-//
-//      if (!result) {                 // same as writing if (result === false)
-//        $(".not").text("not");
-//      } else {
-//        $(".not").text("");
-//      }
-//
-//      $("#result").show();
 //   });
 // });
+//
+//
+// // $(document).ready(function() {
+// //   $("form#leap-year").submit(function(event) {
+// //     event.preventDefault();
+// //     var number = parseInt($("input#number").val());
+// //     var result = leapYear(year);
+// //
+// //     $(".year").text(year);
+// //
+// //      if (!result) {                 // same as writing if (result === false)
+// //        $(".not").text("not");
+// //      } else {
+// //        $(".not").text("");
+// //      }
+// //
+// //      $("#result").show();
+// //   });
+// // });
